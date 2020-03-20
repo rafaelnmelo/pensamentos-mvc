a74a9c0d68ab26e4bd3bfa7e25a35ff00fd63627
Rafael Melo <rafaelnmelo@live.com>
2020-03-20 16:15:47 -0300

    initial commit

---
+++ Pensamentos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by Rafael Melo on 20/03/20.
+//  Copyright © 2020 Rafael Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
